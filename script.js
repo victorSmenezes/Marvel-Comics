8d9d741dc97d87768b8658c7095df5a3c859d711
Dusky7 <victorsantos12dima@gmail.com>
2023-03-31 23:46:06 -0300

    description adc

--- script.js
+++ script.js
@@ -6,65 +6,81 @@ fetch(`http://gateway.marvel.com/v1/public/comics?&ts=${timeStamp}&apikey=${publ
 ).then((response) => {
     return response.json();
 
-}).then(async(jsonParsed) => {
+}).then(async (jsonParsed) => {
     console.log(jsonParsed)
 
     const divComic = document.querySelector('.Comics-Heroes')
-    jsonParsed.data.results.forEach(element => {
+    await jsonParsed.data.results.forEach(element => {
         const Image = element.thumbnail.path + '.' + element.thumbnail.extension
         const Title = element.title
-        
+        const description = element.description
 
-        createComics(Image, Title, divComic)
+        createComics(Image, Title, divComic, description)
     })
 
-/* jsonParsed.data.results.textObjects.forEach(element =>{
-        console.log(element.text)
-    }) */
 })
 
-const createComics = (Logo, title, divToAppend) => {
+const createComics = (Image, title, divToAppend, description) => {
     const comicContainer = document.createElement('div')
     const titleBox = document.createElement('div')
     const titleComic = document.createElement('p')
     const img = document.createElement('img')
 
+    img.src = Image
     titleComic.textContent = title
-    img.src = Logo
 
 
     titleBox.appendChild(img)
     titleBox.appendChild(titleComic)
     comicContainer.appendChild(titleBox)
     divToAppend.appendChild(comicContainer)
+
     comicContainer.classList.add('Struture', 'comic')
     img.classList.add("imgs")
     titleComic.classList.add('text')
 
-    
-  
+    comicContainer.addEventListener('click', () => {
+        Description(Image, title, description);
+    });
 
 }
 
-const Description = (description) => {
-    
+async function Description(Logo, title, description) {
     const modal = document.querySelector('.modal')
     const modalContent = modal.querySelector('.modal-content')
-    const descriptionText = modalContent.querySelector('.description-text')
+    const imgModal = document.createElement('img')
+    const titleModal = document.createElement('h3')
+    const descriptionText = document.createElement('p')
+
+    imgModal.src = Logo
+    if(imgModal.src == 'undefined' ){
+    imgModal.remove() 
+    }
+    titleModal.textContent = title
     
 
+    if (description !== null || undefined) {
+        descriptionText.textContent = description
+    } else {
+        descriptionText.textContent = 'Description unavailable'
+    }
+
+    imgModal.classList.add('imgModal')
+    descriptionText.classList.add('description-text')
+    
+    modalContent.appendChild(imgModal)
+    modalContent.appendChild(titleModal)
+    modalContent.appendChild(descriptionText)
+
     modal.style.display = 'block'
-    descriptionText.textContent = description
 
-
-    const closeButton = document.querySelector('.close-button')
+   const closeButton = document.querySelector('.close-button')
     closeButton.addEventListener('click', () => {
         modal.style.display = 'none'
+        titleModal.remove()
+        imgModal.remove()
+        descriptionText.remove()
     })
 
-    window.onclick = function(event) {
-        if (event.target == modal) {
-            modal.style.display = 'none'
-        }
-    }
 }
+
